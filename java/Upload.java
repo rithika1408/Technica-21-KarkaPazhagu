a62605ec0867628cc1f82fde98529983596f1919
RITHIKA M S <72606939+rithika1408@users.noreply.github.com>
2021-04-11 09:17:04 +0530

    Add files via upload

---
+++ java/Upload.java
@@ -0,0 +1,29 @@
+package com.example.karkapazhagu;
+
+public class Upload {
+
+    public String name;
+    public String url;
+
+
+
+    public Upload() {
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url= url;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+
+
+}
---
+++ java/Uploads.java
@@ -0,0 +1,26 @@
+package com.example.karkapazhagu;
+
+
+public class Uploads {
+    public String description;
+    public  String url;
+
+
+
+    public Uploads() {
+    }
+
+    public Uploads( String description, String url) {
+        this.description=description;
+        this.url=url;
+    }
+
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getUrl(){
+        return  url;
+    }
+}
